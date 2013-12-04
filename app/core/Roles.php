65aed5691f43144574fd78979166aece1da4e738
xi4oh4o <xi4oh4o@gmail.com>
2013-12-04 15:01:27 +0800

    Change comment and Add Autoloader
    modify comment, add require autoloder

--- app/core/Roles.php
+++ app/core/Roles.php
@@ -5,7 +5,20 @@
  * @author Howe Isamu <xi4oh4o@gmail.com>
  */
 /*
+|---------------------
+|register config files
+|---------------------
+*/
+require_once __DIR__.'/../config/config.php';
+/*
 |--------------------
-|Register auto loader
+|register auto loader
 |--------------------
- */
+*/
+require_once 'Autoloder.php';
+/*
+|********
+|Initlize
+|********
+*/
+new Autoloder;
