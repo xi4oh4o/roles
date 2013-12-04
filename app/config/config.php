1e448bdc476f458772853795dc4f91407256c1e4
xi4oh4o <xi4oh4o@gmail.com>
2013-12-04 15:17:06 +0800

    Add VIEW_PATH to config file
    create VIEW Path to config

--- app/config/config.php
+++ app/config/config.php
@@ -12,6 +12,7 @@
 defined( 'APP_PATH' ) or define( 'APP_PATH', $_SERVER['DOCUMENT_ROOT'].'/app/' );
 defined( 'CONF_PATH' ) or define( 'CONF_PATH', APP_PATH.'config/');
 defined( 'CORE_PATH' ) or define( 'CORE_PATH', APP_PATH.'core/');
+defined( 'VIEW_PATH' ) or define( 'VIEW_PATH', APP_PATH.'views/');
 /*
 |------------------
 |database configure
