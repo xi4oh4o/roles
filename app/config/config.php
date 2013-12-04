2e04acc8b32d69674af3ba5e37886bd6cf2dcd71
xi4oh4o <xi4oh4o@gmail.com>
2013-12-04 17:03:57 +0800

    Add Controller and Model constant Path

--- app/config/config.php
+++ app/config/config.php
@@ -12,6 +12,8 @@
 defined( 'APP_PATH' ) or define( 'APP_PATH', $_SERVER['DOCUMENT_ROOT'].'/app/' );
 defined( 'CONF_PATH' ) or define( 'CONF_PATH', APP_PATH.'config/');
 defined( 'CORE_PATH' ) or define( 'CORE_PATH', APP_PATH.'core/');
+defined( 'CONTROLLER_PATH' ) or define( 'CONTROLLER_PATH', APP_PATH.'controllers/');
+defined( 'MODEL_PATH' ) or define( 'MODEL_PATH', APP_PATH.'model/');
 defined( 'VIEW_PATH' ) or define( 'VIEW_PATH', APP_PATH.'views/');
 /*
 |----------
