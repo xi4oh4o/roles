6335d61ccc9496ed89db6ee5a8f5f4a959eda630
xi4oh4o <xi4oh4o@gmail.com>
2013-12-04 11:20:59 +0800

    Initialize and  Add entrance files
    create index.php entrance files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Roles - A users manager system
+ * @package Roles
+ * @author Howe Isamu <xi4oh4o@gmail.com>
+ */
+/*
+|-----------------------
+|Register The Roles Core
+|-----------------------
+*/
+require($_SERVER['DOCUMENT_ROOT'].'/app/Core/Roles.php');
