98c7b9ea95b1ef619562a62d28aae7b3cc725b7d
xi4oh4o <xi4oh4o@gmail.com>
2013-12-04 23:34:55 +0800

    Add User Model
    create User.php files and into someing method

---
+++ app/models/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Roles - User Model
+ * @package Roles
+ * @author Howe Isamu <xi4oh4o@gmail.com>
+ */
+class User
+{
+    static function loginVertify() {
+        echo "fuck";
+        return true;
+    }
+}
+?>
