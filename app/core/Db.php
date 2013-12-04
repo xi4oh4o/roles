acd3e0bf07ce70424c2493b27c89e8e5dc2d585d
xi4oh4o <xi4oh4o@gmail.com>
2013-12-04 16:12:24 +0800

    modify Db Factory class

--- app/core/Db.php
+++ app/core/Db.php
@@ -5,30 +5,30 @@
  * @author Howe Isamu <xi4oh4o@gmail.com>
  */
 //Load Configuration files
-require_once( $_SERVER['DOCUMENT_ROOT'].'/app/config/config/php');
+require_once( $_SERVER['DOCUMENT_ROOT'].'/app/config/config.php' );
 /**
  * Using Static Method return PDO instance
  * @access public
  * @global object $GLOBALS['PDOS'][$Key]
- * @param string $DSN, $User, $Pass, $parms
+ * @param string $strDSN, $strUser, $strPass, $arParms
  * @return instance unique PDO
  */
-class PDOFactory {
+class Db {
     protected $GLOBALS = NULL;
-    public static function GetPDO( $DSN, $User, $Pass, $Parms ) {
-        $Key = md5(serialize(array( $DSN, $User, $Pass, $Parms )));
-        if( !( $GLOBALS['PDOS'][$Key] instanceof PDO)) {
+    public static function GetPDO( $strDSN, $strUser, $strPass, $arParms ) {
+        $strKey = md5( serialize( array( $strDSN, $strUser, $strPass, $arParms ) ) );
+
+        if ( @!$GLOBALS["PDOS"][$strKey] instanceof PDO ) {
             try {
-                $GLOBALS['PDOS'][$Key] = new PDO( $DSN, $User, $Pass, $Parms );
+                $GLOBALS["PDOS"][$strKey] = new PDO( $strDSN, $strUser,
+                $strPass, $arParms );
             } catch ( PDOException $e ) {
-                if (DEBUG_MODE) {
-                    $displayHash['db_error'] = $e->getMessage();
+                if ( DEBUG_MODE ) {
+                    echo $e->getMessage();
                 }
-                require_once( VIEW_PATH."db_faults.php" );
-                exit(0);
+
             }
         }
-        return ( $GLOBALS['PDOS'][$Key] );
+        return ( $GLOBALS["PDOS"][$strKey] );
     }
 }
-?>
