1efe28ae8656a9b915fe5ae9724ddcfb5dd4282a
xi4oh4o <xi4oh4o@gmail.com>
2013-12-04 11:22:21 +0800

    Initial directory structure and Add the User test
    initial directory and add User model unit test

---
+++ app/tests/unit/UserTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * phpUnit UserTest
+ * @package Roles
+ * @usage phpunit app/tests/unit
+ * @author Howe Isamu <xi4oh4o@gmail.com>
+ */
+/*
+|------------------------------------
+| Unit test User model crud function
+|------------------------------------
+ */
+class UserTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+    }
+
+    public function testRead()
+    {
+    }
+
+    public function testUpdate()
+    {
+    }
+
+    public function testDelete()
+    {
+    }
+}
+?>
