add5828e5239a0c23cd1f959fc91a399fab02973
xi4oh4o <xi4oh4o@gmail.com>
2013-12-05 17:41:01 +0800

    Update User model method

--- app/models/User.php
+++ app/models/User.php
@@ -4,11 +4,21 @@
  * @package Roles
  * @author Howe Isamu <xi4oh4o@gmail.com>
  */
+/**
+ * createUser Function
+ * using generate random salt confusing password,
+ * and save confusing password.
+ * @var $salt integer 1/10 Million random Num
+ * @var $hash string md5 encrypt password
+ * @var $salt_hash string salt+hash after md5 encrypt
+ * @author Howe Isamu <xi4oh4o@gmail.com>
+ */
 class User
 {
-    static function loginVertify() {
-        echo "fuck";
-        return true;
+    public static function createUser( $POST ) {
+        $salt      = mt_rand( 100000, 999999 );
+        $hash      = md5( $passwd );
+        $salt_hash = md5( $salt.$hash );
     }
 }
-?>
+
