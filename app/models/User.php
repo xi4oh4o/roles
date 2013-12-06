f43e9e01ef5e9d456108b46eb4a9102154dd5c7b
xi4oh4o <xi4oh4o@gmail.com>
2013-12-06 14:41:46 +0800

    To improve User class
    improve user model

--- app/models/User.php
+++ app/models/User.php
@@ -15,10 +15,23 @@
  */
 class User
 {
-    public static function createUser( $POST ) {
+    public static function createUser( $args ) {
         $salt      = mt_rand( 100000, 999999 );
-        $hash      = md5( $passwd );
+        $hash      = md5( $password );
         $salt_hash = md5( $salt.$hash );
+        $dbh = Db::GetPDO( DB_DSN, DB_USER, DB_PASS, NULL);
+        $insertUser = "INSERT INTO `roles`.`roles_user` (`uid`,
+            `username`, `email`, `bio`, `groups`) VALUES (NULL,
+            '{$username}', '{$email}', '{$bio}', '{$groups}')";
+    }
+
+    public static function readUser() {}
+
+    public static function updateUser() {}
+
+    public static function deleteUser() {}
+
+    public static function vertifyUser( $args ) {
+        
     }
 }
-
