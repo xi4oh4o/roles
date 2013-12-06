88077bf6fc0fca9f3703bce8a374e617e08647da
xi4oh4o <xi4oh4o@gmail.com>
2013-12-06 16:52:14 +0800

    Update Login widget form element
    modify login.php form element

--- app/views/site/login.php
+++ app/views/site/login.php
@@ -1,12 +1,12 @@
 <div id="login">
-<form id="login-form" action="/user/vertify" method="post">
+<form id="login-form" action="">
     <label for="LoginBoxem">Username</label>
-    <input name="username" id="LoginBoxem" type="text" onblur="toolkit.Check.loginNull()">
+    <input name="username" id="LoginBoxem" type="text" onblur="Toolkit.Check.loginNull()">
     <label for="LoginBoxPs">Password</label>
-    <input name="password" id="LoginBoxPs" type="password" onblur="toolkit.Check.loginNull()">
+    <input name="password" id="LoginBoxPs" type="password" onblur="Toolkit.Check.loginNull()">
     <div class="errorMessage" id="LoginErrorMessage"></div>
     <div class="row buttons">
-    <input type="submit" name="loginBtn" value="Login">
+    <input type="button" name="LoginBtn" value=" Login " onclick="Ajax.Query.login()">
     </div>
 </form>
 </div>
