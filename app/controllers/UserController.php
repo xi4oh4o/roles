7f166ea6822a8db14d259b08806d6e981dde49e0
xi4oh4o <xi4oh4o@gmail.com>
2013-12-06 14:44:01 +0800

    Rename JS FileName and add User Controller

---
+++ app/controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Roles - User Controller
+ * @package Roles Controller
+ * @author Howe Isamu <xi4oh4o@gmail.com>
+ */
+class UserController extends Controller
+{
+    function vertifyAction() {
+    }
+}
