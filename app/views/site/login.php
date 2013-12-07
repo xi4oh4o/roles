1339567c0123c99f86e6e9ff6168c32fb3953d4b
xi4oh4o <xi4oh4o@gmail.com>
2013-12-07 19:16:17 +0800

    Enhance the login form
    Add a placeholder etc.

--- app/views/site/login.php
+++ app/views/site/login.php
@@ -1,12 +1,13 @@
 <div id="login">
 <form id="login-form" action="">
-    <label for="LoginBoxem">Username</label>
-    <input name="username" id="LoginBoxem" type="text" onblur="Toolkit.Check.loginNull()">
-    <label for="LoginBoxPs">Password</label>
-    <input name="password" id="LoginBoxPs" type="password" onblur="Toolkit.Check.loginNull()">
+    <h3>Welcome Shinetech</h3>
+    <p><label for="LoginBoxem">Username</label>
+    <input name="username" id="LoginBoxem" type="text" placeholder="demo"></p>
+    <p><label for="LoginBoxPs">Password</label>
+    <input name="password" id="LoginBoxPs" type="password" onblur="Toolkit.Check.loginNull()"></p>
     <div class="errorMessage" id="LoginErrorMessage"></div>
     <div class="row buttons">
-    <input type="button" name="LoginBtn" value=" Login " onclick="Ajax.Query.login()">
+    <input type="button" name="LoginBtn" value=" Log-in " onclick="Ajax.Query.login()">
     </div>
 </form>
 </div>
