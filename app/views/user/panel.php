ffb5518fce897d3b07acdd9b7e2d2d5a722aaf8d
xi4oh4o <xi4oh4o@gmail.com>
2013-12-08 00:03:55 +0800

    Verify the panel access
    verify panel visit access

--- app/views/user/panel.php
+++ app/views/user/panel.php
@@ -1,3 +1,4 @@
+<?php $this->is_login ? true : exit; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,11 +9,17 @@
 </head>
 <body>
     <div id="container">
+    <div id="header">
+    <h3>Users Panel</h3>
+    </div>
+    <div id="user_list">
     <table id="users">
         <tr>
             <td>Not query to the data</td>
         </tr>
     </table>
+    <p>The obtained from Ajax.❤ </p>
+    </div>
     </div>
     <a href="/user/logout"><input type="button" name="logout" id="logoutBtn" value=" Logout " /></a>
     <!-- fetch user list -->
