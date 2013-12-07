aa2f436d9643fee82c582a64838da6b8d57c937d
xi4oh4o <xi4oh4o@gmail.com>
2013-12-08 00:03:22 +0800

    Add login Tips
    add login username password tips

--- app/views/site/login.php
+++ app/views/site/login.php
@@ -8,6 +8,7 @@
     <div class="errorMessage" id="LoginErrorMessage"></div>
     <div class="row buttons">
     <input type="button" name="LoginBtn" value=" Log-in " onclick="Ajax.Query.login()">
+    <h6>hit: demo demo</h6>
     </div>
 </form>
 </div>
