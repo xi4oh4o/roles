94505cd3fafaeef4e6954d2cbab6dcd7a52f8b4b
xi4oh4o <xi4oh4o@gmail.com>
2013-12-08 02:14:20 +0800

    Add User Controller Index action
    Add the default home page

---
+++ app/views/user/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 Not Found - Shinetech Inc.</title>
+</head>
+<body>
+<pre>
+404 Not Found
+Shinetech Inc.
+</pre>
+</body>
+</html>
